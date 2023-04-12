8da7058113203dfd05f2a6e9e714e695cb1d7392
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-12 11:17:20 +0300

    Добавлено решение задачи 5,6.

--- src/Main.java
+++ src/Main.java
@@ -93,7 +93,7 @@ public class Main {
 
     }
     public static void task4 () {
-        System.out.println("\nЗадача 4");
+        System.out.println("\nЗадача №4");
         // Пишем код для задачи 4
         short personAge = 28;
         task4_1(personAge);
@@ -111,18 +111,69 @@ public class Main {
         task4_1(personAge);
     }
 
-    public static void task5 () {
-        System.out.println("\nЗадача 5");
-        // Пишем код для задачи 5
+    public static void task5_1 (short age) {
 
+        if ( age > 14) {
+            System.out.println("Если возраст ребенка равен " + age +
+                    ", то ему можно кататься на аттракционе без сопровождения взрослого.");
+        }
+        else {
+            if ( age > 5 ) {
+                System.out.println("Если возраст ребенка равен " + age +
+                        ", то ему можно кататься на аттракционе в сопровождении взрослого.");
+            }
+            else {
+                   System.out.println("Если возраст человека равен " + age +
+                            ", то ему нельзя кататься на аттракционе.");
+            }
+        }
     }
-    public static void task6 () {
-        System.out.println("\nЗадача 6");
-        // Пишем код для задачи 6
+    public static void task5 () {
+        System.out.println("\nЗадача №5");
+        // Пишем код для задачи 5
+        short personAge = 18;
+        task5_1(personAge);
 
+        personAge = 7;
+        task5_1(personAge);
+
+        personAge = 5;
+        task5_1(personAge);
+    }
+
+    public static void task6_1 (int passengerNumber) {
+
+        if ( passengerNumber > 102) {
+            System.out.println("Всего в вагоне уже 102 пассажира. Свободных мест нет.");
+        }
+        else {
+            if ( passengerNumber > 60 ) {
+                int standPlaces = passengerNumber - 60;
+                System.out.println("В вагоне доступно " + standPlaces +
+                        " стоячих мест. Сидячие места закончились.");
+            }
+            else {
+                int seatPlaces = 60 - passengerNumber;
+                System.out.println("В вагоне доступно " + seatPlaces +
+                        " сидячих и 62 стоячих места.");
+            }
+        }
+    }
+
+    public static void task6 () {
+        System.out.println("\nЗадача №6");
+        // Пишем код для задачи 6
+        int allPassengers = 112;
+        task6_1(allPassengers);
+
+        allPassengers = 93;
+        task6_1(allPassengers);
+
+        allPassengers = 42;
+        task6_1(allPassengers);
     }
     public static void task7 () {
-        System.out.println("\nЗадача 7");
+        System.out.println("\nЗадача №7");
         // Пишем код для задачи 7
     }
 
